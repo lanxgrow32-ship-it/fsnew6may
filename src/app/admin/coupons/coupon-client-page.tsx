690f66d3271f9167a503318a679714dd21ece9a0
Lanxgrow <lanxgrow32@gmail.com>
2025-11-04 18:06:09 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@/

--- src/app/admin/coupons/coupon-client-page.tsx
+++ src/app/admin/coupons/coupon-client-page.tsx
@@ -1,7 +1,8 @@
+
 'use client';
 
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/componentsui/card';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { createCoupon } from './actions';
