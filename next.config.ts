bcb1e86b3ea94e54b61cc3f17aba097663199f9c
Lanxgrow <lanxgrow32@gmail.com>
2025-11-04 19:55:43 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'jxbjdswvrugptnigdguw.supabase.co',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
