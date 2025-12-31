b8feccea9e7374ec19e8ac41cd45ba0486d19406
Lanxgrow <lanxgrow32@gmail.com>
2025-12-31 12:10:58 +0000

    ok wokring great other all is good just here on dpeloiying this version

--- src/app/payment-success/page.tsx
+++ src/app/payment-success/page.tsx
@@ -1,6 +1,6 @@
 
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from '@/components/ui/card';
 import { CheckCircle } from 'lucide-react';
 import Link from 'next/link';
 
