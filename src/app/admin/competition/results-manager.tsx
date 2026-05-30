8586954e7f618ad618b0c392bbabf330d301681b
Lanxgrow <lanxgrow32@gmail.com>
2026-05-30 11:07:22 +0000

    Try fixing this error: `Runtime Error: Error: cn is not defined. Error s

--- src/app/admin/competition/results-manager.tsx
+++ src/app/admin/competition/results-manager.tsx
@@ -12,6 +12,7 @@ import { archiveWeekResults } from './actions';
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from '@/components/ui/alert-dialog';
 import jsPDF from 'jspdf';
 import 'jspdf-autotable';
+import { cn } from '@/lib/utils';
 
 interface jsPDFWithAutoTable extends jsPDF {
   autoTable: (options: any) => jsPDF;
